76dbc18a2769f48feab5bd754f04c9de0668866b
minkman <lomick2090@gmail.com>
2022-07-26 18:40:03 -0500

    Finish 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times < 0) { return 'ERROR';}
+
+    let newString = '';
+
+    for (let i = 0; i < times; i++) {
+
+        newString += string;
+    } 
+    return newString;
 
 };
 
