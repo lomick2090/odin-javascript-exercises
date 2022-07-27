c214e0d1f745a5c4bac5ae666ef28449ff3a19ea
minkman <lomick2090@gmail.com>
2022-07-27 09:28:32 -0500

    Finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(Ftemp) {
+  return Math.round(((Ftemp - 32) * (5/9)*10)) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(Ctemp) {
+  return Math.round(((Ctemp * (9/5)) + 32)*10) / 10;
 };
 
 // Do not edit below this line
