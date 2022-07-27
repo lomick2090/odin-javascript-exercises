8686ccb773899bfad3fd1bcf886f549559f937ac
minkman <lomick2090@gmail.com>
2022-07-27 08:53:01 -0500

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+function searchArray (array, arg) {
+    for (let i = 0; i < array.length; i++) {
+        
+        if (array[i] === arg) {
+            array.splice(i , 1);
+        
+        }
 
+    }
+    return array;
+}
+
+const removeFromArray = function() {
+    const args = Array.from(arguments);
+
+    for (let i = 1; i < args.length ; i++) {
+        var array = searchArray(args[0], args[i]);
+
+    }
+    return array;
 };
 
 // Do not edit below this line
