0491186ee75d46ad98b4db12e0ef497265582ef1
minkman <lomick2090@gmail.com>
2022-08-26 11:58:24 -0500

    Finish lesson 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a- b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  
+  let sum = 0;
+for (let i in array) {
+  sum += array[i];
+}
+return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let mult = 1;
+for (let i in array) {
+  mult *= array[i];
+}
+return mult;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  if (b == 0) { return 0;}
+  var pow = a;
+  for (let i = 1; i < b; i++) {
+    pow = pow * a; 
+    console.log(pow);
+  }
+  return pow;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+
+  let fact = 1;
+  for (let i = 1; i <= a; i ++) {
+    fact = fact * i;
+  }
+  return fact;
 	
 };
 
@@ -31,3 +56,4 @@ module.exports = {
   power,
   factorial
 };
+
