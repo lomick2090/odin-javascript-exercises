723a6e01fba70022c15df6b68ef9712a149e1438
minkman <lomick2090@gmail.com>
2022-08-29 11:12:23 -0500

    Finish lesson 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    var currentYear= new Date().getFullYear();
 
+    const sortedPeeps = people.sort(function(a, b) {
+        
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = currentYear; 
+        } else if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = currentYear;
+        }
+    
+        return ((a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) ? 1 : -1);
+    
+    });
+    return sortedPeeps[0];
 };
 
 // Do not edit below this line
