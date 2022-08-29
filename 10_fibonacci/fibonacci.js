e626e3c266756828da7783e0c91e63fde5d78d07
minkman <lomick2090@gmail.com>
2022-08-29 10:21:53 -0500

    Finish lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {return 'OOPS'};
+    let array = [0,1,1];
+    for (i = 3; i <= num; i++){
+        array[i] = array[(i-1)] + array[(i-2)]
+    }
+    return array[num];
 };
 
 // Do not edit below this line
